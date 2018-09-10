658966bf42b68c541fae61ed6e364fa0ce57f54a
Jeremy D Rhodes <jdrhodes@ecc-jaguar.csuchico.edu>
2018-09-10 16:44:03 -0700

    Description of what this version does

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -4,15 +4,17 @@
 int main()
 {
   //declare variables here
+  int num;
+  int bignum;
 
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
-
+  std::cin>>num;
   std::cout<<"Please enter another whole number:\n";
   //get second input using std::cin>> again with a second variable
-
+  std::cin>>bignum;
   //determine if first or second is bigger and store that in a third variable
-
+  
 
   std::cout<<"Of those two numbers, the biggest is: ";
   //print out the variable representing the biggest number
