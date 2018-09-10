1347afdea4c49be40115c23e9b7a799c39266240
Jeremy D Rhodes <jdrhodes@ecc-jaguar.csuchico.edu>
2018-09-10 16:52:51 -0700

    Finished Lab

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,7 +6,7 @@ int main()
   //declare variables here
   int num;
   int bignum;
-
+  int biggest;
   std::cout<<"Please enter a whole number:\n";
   //get first input using std::cin>>
   std::cin>>num;
@@ -14,9 +14,18 @@ int main()
   //get second input using std::cin>> again with a second variable
   std::cin>>bignum;
   //determine if first or second is bigger and store that in a third variable
-  
+  if(num > bignum)
+  {
+          biggest = num;
+  }
+  else
+  {
+          biggest = bignum;
+  }
+
 
   std::cout<<"Of those two numbers, the biggest is: ";
+  std::cout<<biggest;
   //print out the variable representing the biggest number
 
   std::cout<<std::endl<<"Thank you for playing.\n";
